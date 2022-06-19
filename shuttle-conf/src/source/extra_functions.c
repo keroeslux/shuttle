b68f7d01f6073b6892d2107fede5c3712f757680
sus <sus@sus>
2022-06-19 16:53:10 +0000

    Fixed smthin

--- shuttle-conf/src/source/extra_functions.c
+++ shuttle-conf/src/source/extra_functions.c
@@ -1,10 +1,18 @@
 #include <sys/stat.h>
 #include <stdbool.h>
-
+#include "../include/colors.h"
 extern bool check_for_file(const char *path)
 {
     struct stat buff;
     int cc = stat(path, &buff);
     if (cc==0) {return true; }
     else {return false; }
+}
+
+
+void crash(char *str)
+{
+    red();
+    printf(str);
+    abort();
 }
