dde80d97c1e303fd970007f5f68031becd287286
sus <sus@sus>
2022-06-20 16:29:26 +0000

    Added the framework for a multi-argument parsing function

--- shuttle-conf/src/include/argparse.h
+++ shuttle-conf/src/include/argparse.h
@@ -52,4 +52,46 @@ char *parse_arg(int size, char **argv, char *flag_short, char *flag_long)
     }
     return (char *)EXIT_FAILURE;
 }
+char *parse_args(int size, char **argv, char *flag_short, char *flag_long, int count)
+{
+    char *args[sizeof(argv) / sizeof(argv[0])];
+    char *ptr;
+    if((argv[0]) == NULL)
+    {
+        exit(1);
+    }
+    else
+    {
+        int temp = 0;
+        while(temp<size)
+        {
+            if(strcmp(argv[temp],flag_short)==0 || strcmp(argv[temp], flag_long)==0)
+            {
+                temp+=1;
+                if(temp>=size)
+                {
+                    exit(1);
+                }
+                else
+                {
+                    int i;
+                    while(i<count)
+                    {
+                        args[i] = argv[temp];
+                        temp+=1;
+                        i+=1;
+                    }
+                    ptr = &args;
+                    return ptr;
+                }
+            }
+            else
+            {
+                ++temp;
+            }
+        }
+    }
+    return (char *)EXIT_FAILURE;
+}
+
 #endif
