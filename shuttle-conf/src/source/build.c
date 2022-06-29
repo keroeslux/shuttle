5ffbc75f1e39296f41c08ad672fff4f6af820442
keroeslux <keroeslux@keroeslux-artix>
2022-06-29 02:01:34 +0000

    fixed first version

--- shuttle-conf/src/source/build.c
+++ shuttle-conf/src/source/build.c
@@ -1,8 +1,21 @@
 #include <stdbool.h>
+#include <stdlib.h>
 #include "../include/program_structs.h"
 #include "../include/parse.h"
 #include "../include/extra_functions.h"
 #define count arr_len(structs)
+
+void build(char **arr) {}
+
+void dump_arr(char **arr)
+{
+    int i = 0;
+    while(arr[i] != NULL)
+    {
+        printf("%s, ", arr[i]);
+        i++;
+    }
+}
 
 void can_build(const char *path, bool force)
 {
@@ -15,4 +28,26 @@ void can_build(const char *path, bool force)
             arr[i] = structs[i];
         }
     }
+    if (force == true)
+    {
+        build(arr);
+    }
+    else
+    {
+        char buff[3];
+        printf("Found the following: %s :: continue? [y\\n]: ", dump_arr(arr));
+        fgets(buff, 3, stdin);
+        if (buff == 'y')
+        {
+            build(arr);
+        }
+        else
+        {
+            abort();
+        }
+    }
+}
+}
+}
+}
 }    
